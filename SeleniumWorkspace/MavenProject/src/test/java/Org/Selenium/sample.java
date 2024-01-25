0d77ee1ed44d2bc4c23cdd746573aaac6e05149c
Subasri Sampath <subasri.sampath@photon.com>
2024-01-25 17:05:45 +0530

    code update [skip ci]

--- SeleniumWorkspace/MavenProject/src/test/java/Org/Selenium/sample.java
+++ SeleniumWorkspace/MavenProject/src/test/java/Org/Selenium/sample.java
@@ -9,12 +9,15 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class sample {
 	public static WebDriver driver;
 	public static void main(String[] args) throws Throwable {
-//		WebDriverManager.chromedriver().setup();
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--remote-allow-origins=*");
-//        WebDriver driver = new ChromeDriver(options);
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\Driver\\chromedriver.exe");
-		driver = new ChromeDriver();
+		WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+		/*
+		 * System.setProperty("webdriver.chrome.driver",
+		 * System.getProperty("user.dir")+"\\Driver\\chromedriver.exe"); driver = new
+		 * ChromeDriver();
+		 */
 		driver.get("https://google.com");
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
